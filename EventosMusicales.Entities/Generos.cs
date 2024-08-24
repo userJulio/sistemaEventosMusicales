c4407c1e77e2bf0d9ff9c97453b9f3eba9d921b1
Julio Chayan <julioch1200@gmail.com>
2024-08-23 21:33:19 -0500

    Initial

---
+++ EventosMusicales.Entities/Generos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventosMusicales.Entities
+{
+    public class Generos
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public bool Estado { get; set; } = true;
+    }
+}
---
+++ EventosMusicales.Repositories/GeneroRepositorio.cs
@@ -0,0 +1,60 @@
+﻿using EventosMusicales.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventosMusicales.Repositories
+{
+    public class GeneroRepositorio
+    {
+        private readonly List<Generos> listGeneros;
+        public GeneroRepositorio()
+        {
+            listGeneros=new List<Generos>();
+            listGeneros.Add(new Generos() { Id = 1, Name = "Salsa" });
+            listGeneros.Add(new Generos() { Id = 2, Name = "Cumbia" });
+            listGeneros.Add(new Generos() { Id = 3, Name = "Rock" });
+
+        }
+
+        public List<Generos> GetGeneros()
+        {
+            return listGeneros;
+        }
+
+        public Generos? GetGeneros(int id)
+        {
+            var item= listGeneros.FirstOrDefault(x => x.Id == id);
+            return item;
+        }
+        public Generos Add(Generos gener)
+        {
+            var lastItem = listGeneros.MaxBy(x => x.Id);
+            gener.Id = lastItem is null ? 1: lastItem.Id +1;
+            listGeneros.Add(gener);
+            return gener;
+
+        }
+        public void Update(int id, Generos genre)
+        {
+            var item = GetGeneros(id);
+            if(item is not null)
+            {
+                item.Name = genre.Name;
+                item.Estado = genre.Estado;
+            }
+            
+        }
+        public void Delete(int id)
+        {
+            var item = GetGeneros(id);
+            if(item is not null)
+            {
+                listGeneros.Remove(item);
+            }
+        }
+
+    }
+}
